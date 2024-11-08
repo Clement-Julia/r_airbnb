9032ed2db50ab9ec80b91ad2a0c0fd99ff349b85
Clément Julia <clement.julia1708@gmail.com>
2024-11-08 09:12:29 +0100

    example predict use

--- prediction.r
+++ prediction.r
@@ -1,11 +1,21 @@
-install.packages("onnxruntime")
-library(onnxruntime)
+if(!require(xgboost)) install.packages("xgboost")
+library(xgboost)
 
-session <- onnxruntime::InferenceSession("xgboost_model.onnx")
+rm(list = ls())
 
-new_data <- matrix(c(...), nrow=1)
-colnames(new_data) <- c("col1", "col2", ...)
+model <- xgb.load("xgboost_model.bin")
+df <- read.csv("Data/listings_merged.csv")
 
-results <- session$run(list(float_input = new_data))
-prediction <- results[[1]]
-print(prediction)
+selected_columns <- c("property_type", "room_type", "accommodates", "amenities", "neighbourhood_cleansed", "review_scores_rating", "price", "beds", "bathrooms")
+test_data <- df[sample(nrow(df), 3), selected_columns]
+test_data$beds_and_baths <- test_data$beds + test_data$bathrooms
+test_data$price_per_room <- test_data$price / test_data$beds_and_baths
+View(test_data)
+
+test_data <- subset(test_data, select = -c(price, beds, bathrooms))
+
+test_data[] <- lapply(test_data, function(x) if(is.character(x)) as.numeric(as.factor(x)) else x)
+
+data_matrix <- xgb.DMatrix(data = as.matrix(test_data))
+predictions <- predict(model, data_matrix)
+print(predictions)
